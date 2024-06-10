07e684513b6e4b3db4bd1d67b49d47e6e87e1dba
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-06-10 11:53:07 +0100

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,12 +11,14 @@
 
 binary_tree_t	*binary_tree_node(binary_tree_t *parent, int value)
 {
-	parent = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (!parent)
+	binary_tree_t *new_node;
+
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!new_node)
 		return (NULL);
-	parent->n = value;
-	parent->parent = parent;
-	parent->left = NULL;
-	parent->right = NULL;
-	return (parent);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
