9919c2361498d7fba0637b2278b7d9a7289505b2
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-06-10 12:18:35 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *ptr;
 
+    if (!parent)
+        return (NULL);
     if (!parent->left)
         parent->left = binary_tree_node(parent, value);
     else
