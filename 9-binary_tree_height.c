bd4a191a0f0bdf557f2310b9c1cfe1fbf39bcb7f
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-06-12 17:24:58 +0100

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,9 +18,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left_height = binary_tree_height(tree->left) +1;
+		left_height = binary_tree_height(tree->left) + 1;
 	if (tree->right)
-	right_height = binary_tree_height(tree->right) +1;
+	right_height = binary_tree_height(tree->right) + 1;
 	if (left_height > right_height)
 		return (left_height);
 	return (right_height);
