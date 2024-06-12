ece1404c35953d23baa986d241f0d57fba814dd0
houdaifa-boukhari <houdaifa.elboukhari@etu.uae.ac.ma>
2024-06-12 12:39:30 +0100

    task 3

--- binary_trees.h
+++ binary_trees.h
@@ -13,7 +13,7 @@ typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
-    struct binary_tree_s *left;
+    struct binary_tree_s *left;                                                                               
     struct binary_tree_s *right;
 }                        binary_tree_t;
 
@@ -24,6 +24,7 @@ void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);  
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 
 # endif
